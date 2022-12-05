ad5239a592b6d8211f9aaf3d1a34a8057346df58
LiamFenneman <liamfennemanbusiness@gmail.com>
2022-12-05 16:57:01 +1300

    Add rust keybinds to neovim

--- nvim/lua/liam/remaps.lua
+++ nvim/lua/liam/remaps.lua
@@ -1,9 +1,14 @@
 local keymap = require("liam.keymap");
-
 local nnoremap = keymap.nnoremap;
 
 vim.g.mapleader = " "
 nnoremap("<leader>qq", "<cmd>Ex<CR>");
 
+-- Auto center screen when paging up/down
 nnoremap("<C-d>", "<C-d>zz");
 nnoremap("<C-u>", "<C-u>zz");
+
+-- Rust
+nnoremap("<leader>rr", "<cmd>! cargo run<CR>");
+nnoremap("<leader>rc", "<cmd>! cargo check<CR>");
+nnoremap("<leader>rt", "<cmd>! cargo test<CR>");
