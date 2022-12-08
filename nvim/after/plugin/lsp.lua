68b509526c1722a2143311668219706ef0385742
LiamFenneman <liamfennemanbusiness@gmail.com>
2022-12-09 11:52:02 +1300

    Keybinds for nvim LSP

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -12,6 +12,17 @@ lsp.ensure_installed({
     "rust_analyzer",
 })
 
+local cmp = require("cmp")
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+lsp.setup_nvim_cmp({
+    mapping = lsp.defaults.cmp_mappings({
+        ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+        ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+        ['<C-y>'] = cmp.mapping.confirm(cmp_select),
+        ['<C-Space>'] = cmp.mapping.complete(),
+    })
+})
+
 lsp.set_preferences({
     sign_icons = {
         error = 'E',
@@ -22,11 +33,13 @@ lsp.set_preferences({
 })
 
 lsp.on_attach(function (client, bufnr)
-    local noremap = {buffer = bufnr, remap = false}
+    local opts = {buffer = bufnr, remap = false}
     local bind = vim.keymap.set
 
-    bind("n", "R", vim.lsp.buf.rename, noremap)
-    bind("n", "ca", vim.lsp.buf.code_action, noremap)
+    bind("n", "R", vim.lsp.buf.rename, opts)
+    bind("n", "ca", vim.lsp.buf.code_action, opts)
+    bind("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    bind("i", "<C-h>", vim.lsp.buf.signature_help, opts)
 end)
 
 lsp.nvim_workspace()
