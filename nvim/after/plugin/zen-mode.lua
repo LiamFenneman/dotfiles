3508d8771b54adb2f60fd8c8572319c9bd0412c2
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-01-15 12:16:11 +1300

    fix: Zen Mode transparent backround

--- nvim/after/plugin/zen-mode.lua
+++ nvim/after/plugin/zen-mode.lua
@@ -15,6 +15,9 @@ zen.setup({
             font = "+4",
         },
     },
+    on_open = function (win)
+        vim.api.nvim_set_hl(0, "ZenBg", { ctermbg = 0 })
+    end
 })
 
 local nnoremap = require("liam.keymap").nnoremap;
