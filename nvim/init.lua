e57c9834dbf3a215c286a096020c15602ffdea52
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-02-01 05:39:50 +1300

    nvim: add descriptions to harpoon keymaps

--- nvim/init.lua
+++ nvim/init.lua
@@ -218,12 +218,12 @@ require('harpoon').setup({
 })
 local ui = require('harpoon.ui')
 local mark = require('harpoon.mark')
-vim.keymap.set({ 'n', 'v' }, '<leader>a', mark.add_file)
-vim.keymap.set({ 'n', 'v' }, '<leader>e', ui.toggle_quick_menu)
-vim.keymap.set({ 'n', 'v' }, '<leader>1', function() ui.nav_file(1) end)
-vim.keymap.set({ 'n', 'v' }, '<leader>2', function() ui.nav_file(2) end)
-vim.keymap.set({ 'n', 'v' }, '<leader>3', function() ui.nav_file(3) end)
-vim.keymap.set({ 'n', 'v' }, '<leader>4', function() ui.nav_file(4) end)
+vim.keymap.set({ 'n', 'v' }, '<leader>a', mark.add_file, { desc = '[A]dd marker' })
+vim.keymap.set({ 'n', 'v' }, '<leader>e', ui.toggle_quick_menu, { desc = '[E]dit marker menu' })
+vim.keymap.set({ 'n', 'v' }, '<leader>1', function() ui.nav_file(1) end, { desc = 'Go to marker [1]' })
+vim.keymap.set({ 'n', 'v' }, '<leader>2', function() ui.nav_file(2) end, { desc = 'Go to marker [2]' })
+vim.keymap.set({ 'n', 'v' }, '<leader>3', function() ui.nav_file(3) end, { desc = 'Go to marker [3]' })
+vim.keymap.set({ 'n', 'v' }, '<leader>4', function() ui.nav_file(4) end, { desc = 'Go to marker [4]' })
 
 -- [[ Treesitter Config ]]
 require('nvim-treesitter.configs').setup({
@@ -462,4 +462,3 @@ cmp.setup({
         { name = 'luasnip' },
     },
 })
--- [[ ]]
