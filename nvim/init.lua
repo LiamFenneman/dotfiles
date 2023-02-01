62f7e38e88f23a94e460c2d1601f6746d29e3eec
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-02-02 02:28:44 +1300

    nvim: disable treesitter keymap conflict
    Incremental selection causes a keymap conflict with `nvim-cmp`.

--- nvim/init.lua
+++ nvim/init.lua
@@ -281,7 +281,7 @@ require('nvim-treesitter.configs').setup({
         additional_vim_regex_highlighting = false,
     },
     incremental_selection = {
-        enable = true,
+        enable = false,
         keymaps = {
             init_selection = '<c-space>',
             node_incremental = '<c-space>',
