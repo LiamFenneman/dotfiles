0be5186496dccb4ecaab8b336297728b5daf1094
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-03-04 05:28:41 +1300

    nvim: latex different options for autocompile

--- nvim/lua/liam/latex.lua
+++ nvim/lua/liam/latex.lua
@@ -1,19 +1,17 @@
 local group = vim.api.nvim_create_augroup('PdfLatex', { clear = true })
-local id = nil
 
-vim.api.nvim_create_user_command('PdfLatexEnable', function()
-    id = vim.api.nvim_create_autocmd('BufWritePost', {
+vim.api.nvim_create_user_command('LatexMake', function()
+    vim.api.nvim_create_autocmd('BufWritePost', {
         command = 'silent !make clean all',
         group = group,
         pattern = '*.tex',
     })
-    print('PdfLatex enabled')
 end, {})
 
-vim.api.nvim_create_user_command('PdfLatexDisable', function()
-    if id ~= nil then
-        vim.api.nvim_del_autocmd(id)
-        id = nil
-        print('PdfLatex disabled')
-    end
+vim.api.nvim_create_user_command('LatexPdf', function()
+    vim.api.nvim_create_autocmd('BufWritePost', {
+        command = 'silent !pdflatex %',
+        group = group,
+        pattern = '*.tex',
+    })
 end, {})
