c4e1da8ccf23c6626b0ccab569c22ad55c0b1a6e
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-24 15:43:29 +1200

    nvim: add bindings for hook 5-9

--- nvim/after/plugin/hooks.lua
+++ nvim/after/plugin/hooks.lua
@@ -11,6 +11,11 @@ require('hooks').setup({
             [2] = '<leader>2',
             [3] = '<leader>3',
             [4] = '<leader>4',
+            [5] = '<leader>5',
+            [6] = '<leader>6',
+            [7] = '<leader>7',
+            [8] = '<leader>8',
+            [9] = '<leader>9',
         },
     },
 })
