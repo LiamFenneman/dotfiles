800ba857a481872e9b25060ef6b78bbfe6f4d44a
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-28 09:33:14 +1200

    nvim: add keymaps

--- nvim/lua/liam/keymap.lua
+++ nvim/lua/liam/keymap.lua
@@ -15,4 +15,10 @@ vim.keymap.set('n', '<leader>gc', ':Git commit<CR>', { desc = '[G]it [C]ommit' }
 vim.keymap.set('n', '<leader><leader>x', ':source %<CR>', { desc = '[S]ource current file' })
 vim.keymap.set('n', '<leader><leader>r', ':!cargo r<CR>', { desc = 'Cargo [R]un' })
 
+-- Format file
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = '[F]ormat file' })
+
+-- Misc. Tricks
+vim.keymap.set('n', 'YY', 'va{Vy', { desc = '[Y]ank between brackets' })
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv", { desc = 'Move line down' })
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv", { desc = 'Move line up' })
