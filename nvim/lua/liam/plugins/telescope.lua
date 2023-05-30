bd901535dad62206f4ef6ab1afc1f331207a8dbd
LiamFenneman <liamfennemanbusiness@gmail.com>
2023-05-30 21:38:43 +1200

    nvim: git features

--- nvim/lua/liam/plugins/telescope.lua
+++ nvim/lua/liam/plugins/telescope.lua
@@ -17,21 +17,28 @@ return {
             -- Enable telescope fzf native, if installed
             pcall(require('telescope').load_extension, 'fzf')
 
+            local function map(keys, func, desc)
+                if desc then
+                    desc = 'Telescope: ' .. desc
+                end
+                vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+            end
+
             -- See `:help telescope.builtin`
             local ts = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>?', ts.oldfiles, { desc = '[?] Find recently opened files' })
-            vim.keymap.set('n', '<leader><space>', ts.buffers, { desc = '[ ] Find existing buffers' })
+            map('<leader>?', ts.oldfiles, '[?] Find recently opened files')
+            map('<leader><space>', ts.buffers, '[ ] Find existing buffers')
 
-            vim.keymap.set('n', '<leader>sf', ts.find_files, { desc = '[S]earch [F]iles' })
-            vim.keymap.set('n', '<leader>sh', ts.help_tags, { desc = '[S]earch [H]elp' })
-            vim.keymap.set('n', '<leader>sw', ts.grep_string, { desc = '[S]earch current [W]ord' })
-            vim.keymap.set('n', '<leader>sg', ts.live_grep, { desc = '[S]earch by [G]rep' })
-            vim.keymap.set('n', '<leader>sd', ts.diagnostics, { desc = '[S]earch [D]iagnostics' })
-            vim.keymap.set('n', '<leader>sd', ts.diagnostics, { desc = '[S]earch [D]iagnostics' })
+            map('<leader>sf', ts.find_files, '[S]earch [F]iles')
+            map('<leader>sh', ts.help_tags, '[S]earch [H]elp')
+            map('<leader>sw', ts.grep_string, '[S]earch current [W]ord')
+            map('<leader>sg', ts.live_grep, '[S]earch by [G]rep')
+            map('<leader>sd', ts.diagnostics, '[S]earch [D]iagnostics')
+            map('<leader>sk', ts.keymaps, '[S]earch [K]eymaps')
 
-            vim.keymap.set('n', '<leader>sc', ts.git_commits, { desc = '[S]earch Git [C]ommits' })
-            vim.keymap.set('n', '<leader>sbc', ts.git_bcommits, { desc = '[S]earch [B]uffer Git [C]ommits' })
-            vim.keymap.set('n', '<leader>ss', ts.git_status, { desc = '[S]earch Git [S]tatus' })
+            map('<leader>sc', ts.git_commits, '[S]earch Git [C]ommits')
+            map('<leader>sbc', ts.git_bcommits, '[S]earch [B]uffer Git [C]ommits')
+            map('<leader>ss', ts.git_status, '[S]earch Git [S]tatus')
         end,
     },
     { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
