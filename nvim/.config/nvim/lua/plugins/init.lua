d9802be6ec3b63af7accea01bd7f734cb0c21d4a
LiamFenneman <liamfennemanbusiness@gmail.com>
2024-05-25 16:53:50 +1200

    chore: remove unused parts

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -42,41 +42,4 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = { signs = false },
   },
-
-  -- {
-  --     "folke/which-key.nvim",
-  --     event = "VimEnter",
-  --     config = function()
-  --         require("which-key").setup()
-  --         require("which-key").register({
-  --             ["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
-  --             ["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
-  --             ["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
-  --             ["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
-  --             ["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
-  --         })
-  --     end,
-  -- },
-
-  -- Collection of various small independent plugins/modules
-  --  See: https://github.com/echasnovski/mini.nvim
-  {
-    "echasnovski/mini.nvim",
-    config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]parenthen
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require("mini.ai").setup({ n_lines = 500 })
-
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
-      require("mini.surround").setup()
-    end,
-  },
 }
